1108924e97dea1744c9a2d5a68ec9ffd3a7fa203
Jasiel Espinosa <jaszespi@gmail.com>
2017-09-28 16:58:36 +0800

    List View Update Design > Text Color and Format

--- app/src/main/java/com/espinosa/espinosaquiz1/Activity/MainActivity.java
+++ app/src/main/java/com/espinosa/espinosaquiz1/Activity/MainActivity.java
@@ -4,16 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import com.espinosa.espinosaquiz1.R;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView avList;
+    String androidVersions[] = {"Press 1 to select Jelly Bean", "Press 2 to select Kitkat", "Press 3 to select Marshmallow"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        avList = (ListView) findViewById(R.id.lvAndroidVersions);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.activity_listview, R.id.textView, androidVersions);
+        avList.setAdapter(arrayAdapter);
+
     }
 
     public void jellybeanIntent(View view) {
